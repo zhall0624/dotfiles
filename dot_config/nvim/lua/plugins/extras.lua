c783f1d470016043880e028ba16b4a5972631aeb
Zach Hall <zach.hall@hey.com>
2024-04-25 23:22:36 -0700

    nvim and i3 configs

---
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -0,0 +1,9 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.ruby" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+}
