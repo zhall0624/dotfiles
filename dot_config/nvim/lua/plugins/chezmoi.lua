40bae023131163347e6e3e103d2bb5262bc44c50
Zach Hall <zhall@syntaxdata.com>
2024-05-03 15:53:40 -0700

    Add plugins for autosave and highlighting for chezmoi in nvim

---
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "xvzc/chezmoi.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("chezmoi").setup({
+        -- your configurations
+      })
+      local telescope = require("telescope")
+      telescope.load_extension("chezmoi")
+
+      vim.keymap.set("n", "<leader>cz", telescope.extensions.chezmoi.find_files, {})
+    end,
+  },
+  {
+    "alker0/chezmoi.vim",
+    lazy = false,
+    init = function()
+      -- This option is required.
+      vim.g["chezmoi#use_tmp_buffer"] = true
+      -- add other options here if needed.
+      require("nvim-treesitter.configs").setup({
+        highlight = {
+          disable = function()
+            -- check if 'filetype' option includes 'chezmoitmpl'
+            if string.find(vim.bo.filetype, "chezmoitmpl") then
+              return true
+            end
+          end,
+        },
+      })
+    end,
+  },
+}
