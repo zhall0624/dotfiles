6548ac0c24c41270b78d9618d5d57a19abf34c64
Zach Hall <zach.hall0624@pm.me>
2026-01-18 20:39:06 -0800

    Redo Nvim

--- dot_config/nvim/lua/plugins/disabled.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { "echasnovski/mini.pairs", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "echasnovski/mini.comment", enabled = false },
-  { "folke/neodev.nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-}
